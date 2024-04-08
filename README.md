9e855b41a58a9077adaf7f985e4837fca9be4e88
xz <zun_xie@163.com>
2024-04-08 18:55:25 +0800

    多语言方法

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# jogo9k_web_common
-游戏公共工具库
+# i18n
+1,项目后续会对接多国家多币种,需要多语言支持.用i18x表格维护.地址: https://docs.google.com/spreadsheets/d/1wPUMEnBgbEoar_7DTnJh8C5bGjAFfvC1B1la9epsHMQ/edit#gid=0.到此地址取多语言key;
+2,运营产品对多语言修改需求较为频繁,避免每次更改前端需要重新打包改为获取线上json
+3,注意:i18njs中getOnlineJson 要在页面渲染前执行完;
---
+++ i18n/index.js
@@ -0,0 +1,109 @@
+let wordDict = {};
+export const I18_JSON_URL = "https://jogo9k.s3.sa-east-1.amazonaws.com/Dynamic+files/base.json";
+export const LANG_ZH = 'ZH';
+export const LANG_EN = 'EN';
+export const LANG_PT = 'PT';
+
+const reVar = /\{\w*\}/g;
+const defaultLang = LANG_PT;
+const preCache = {};
+let currLang = defaultLang;
+export const setWordDict = (jsonData) => {
+    wordDict = Object.assign(wordDict, jsonData);
+}
+export async function getOnlineJson() {
+    let data = await fetch(I18_JSON_URL).then(response => response.json());
+
+    setWordDict(data);
+}
+
+export function setLanguage() {
+    localStorage.setItem('language', lang);
+    currLang = lang;
+}
+
+export function getLanguage() {
+    if (currLang === undefined) {
+        currLang = localStorage.getItem('language');
+        if (currLang === null || !allLangs.includes(currLang)) {
+            currLang = defaultLang;
+        }
+    }
+    return currLang;
+}
+
+export function tr(key, dict) {
+    const lang = getLanguage();
+    let temp = getAttrs(preCache, key, lang || '');
+
+    if (temp === undefined) {
+        let word = getAttrs(wordDict, key, lang || '');
+        if (word === undefined) {
+            // console.info(`i18n: [${key}](${lang}) not exist`);
+            return key;
+        }
+        temp = preprocess(word);
+        // put cache
+        if (!preCache.hasOwnProperty(key)) {
+            preCache[key] = {};
+        }
+        preCache[key][lang] = temp;
+    }
+    // return key
+    if (temp !== undefined) {
+        return genStr(temp, dict);
+    } else {
+        return key;
+    }
+}
+
+function genStr(temp, dict) {
+    if (temp.temp.length === 1) {
+        return temp.temp[0];
+    } else {
+        const newlist = temp.temp.slice();
+        for (let k in temp.vars) {
+            const pos = temp.vars[k];
+            for (let p of pos) {
+                newlist[p] = dict[k].toString() || '';
+            }
+        }
+        return newlist.join('');
+    }
+}
+function preprocess(temp) {
+    const out = {
+        temp: [],
+        vars: {},
+    };
+    const matches = temp.matchAll(reVar);
+    let pos = 0;
+    for (let match of matches) {
+        const find = match[0];
+        const key = find.slice(1, -1);
+        if (match.index > pos) {
+            out.temp.push(temp.slice(pos, match.index));
+        }
+        pos = match.index + find.length;
+        if (out.vars[key] === undefined) {
+            out.vars[key] = [];
+        }
+        out.vars[key].push(out.temp.length);
+        out.temp.push('');
+    }
+    if (pos < temp.length) {
+        out.temp.push(temp.slice(pos));
+    }
+    return out;
+}
+export function getAttrs(obj, ...keys) {
+    let curr = obj;
+    for (const k of keys) {
+        if (curr.hasOwnProperty(k)) {
+            curr = curr[k];
+        } else {
+            return undefined;
+        }
+    }
+    return curr;
+}
